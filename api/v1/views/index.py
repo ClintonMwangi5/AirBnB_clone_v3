a639175bd0b9b2075637eba058c4cec8b282f1ad
root <root@DESKTOP-I6FTCIN.localdomain>
2025-02-19 00:15:25 +0300

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Index view for the AirBnB API.
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -13,3 +14,20 @@ def status():
     Returns the status of the API.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    Retrieves the number of each object by type.
+    """
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    counts = {key: storage.count(eval(cls)) for key, cls in classes.items()}
+    return jsonify(counts)

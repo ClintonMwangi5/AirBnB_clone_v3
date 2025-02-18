a507373eddaf9506d721f3c10f1091c99ccdd0de
root <root@DESKTOP-I6FTCIN.localdomain>
2025-02-19 01:40:08 +0300

    done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,4 +7,3 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Importing views
 from api.v1.views.index import *
 from api.v1.views.states import *  # Import the new states file#!/usr/bin/python3
-from api.v1.views.city import *

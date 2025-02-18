40bb49c181e110686e1e4f61dbedb7a19e16cf49
root <root@DESKTOP-I6FTCIN.localdomain>
2025-02-19 01:14:19 +0300

    done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,17 @@
+  
 #!/usr/bin/python3
-"""
-Blueprint setup for API views.
-"""
-
+"""Init file for views module"""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Import all views here
-from api.v1.views.index import *  # noqa
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *

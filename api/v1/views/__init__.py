c6e68c3e432e971ea73891c5e7931777d8d20118
root <root@DESKTOP-I6FTCIN.localdomain>
2025-02-19 01:28:09 +0300

    done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""
-Initialize the views module
-"""
+"""Initialize the app_views Blueprint"""
 from flask import Blueprint
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *  # Add this import
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Importing views
+from api.v1.views.index import *
+from api.v1.views.states import *  # Import the new states file#!/usr/bin/python3
+"""Initialize the app_views Blueprint"""
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Importing views
+from api.v1.views.index import *
+from api.v1.views.states import *  # Import the new states file
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,71 +1,64 @@
 #!/usr/bin/python3
-"""Moduli ya Jimbo"""
+"""State objects view - Handles all default RESTful API actions"""
+from flask import jsonify, request, abort
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
-from flasgger.utils import swag_from
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get.yml', methods=['GET'])
-def get_all():
-    """Pata yote kwa kitambulisho"""
-    all_list = [obj.to_dict() for obj in storage.all(State).values()]
-    return jsonify(all_list)
+def get_states():
+    """Retrieve the list of all State objects"""
+    states = storage.all(State)
+    return jsonify([state.to_dict() for state in states.values()])
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'],
-                 strict_slashes=False)
-@swag_from('documentation/state/get_id.yml', methods=['GET'])
-def get_method_state(state_id):
-    """Pata jimbo kwa kitambulisho"""
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieve a specific State object by id"""
     state = storage.get(State, state_id)
-    if state is None:
+    if not state:
         abort(404)
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-@swag_from('documentation/state/delete.yml', methods=['DELETE'])
-def del_method(state_id):
-    """Futa jimbo kwa kitambulisho"""
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """Delete a specific State object by id"""
     state = storage.get(State, state_id)
-    if state is None:
+    if not state:
         abort(404)
-    state.delete()
+    storage.delete(state)
     storage.save()
-    return jsonify({})
+    return jsonify({}), 200
 
 
-@app_views.route('/states/', methods=['POST'],
-                 strict_slashes=False)
-@swag_from('documentation/state/post.yml', methods=['POST'])
-def create_obj():
-    """Unda mfano mpya"""
-    if not request.get_json():
-        return make_response(jsonify({"error": "Si JSON"}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({"error": "Jina linakosekana"}), 400)
-    js = request.get_json()
-    obj = State(**js)
-    obj.save()
-    return jsonify(obj.to_dict()), 201
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Create a new State"""
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'],
-                 strict_slashes=False)
-@swag_from('documentation/state/put.yml', methods=['PUT'])
-def post_method(state_id):
-    """Sasisha kwa kutumia njia ya POST"""
-    if not request.get_json():
-        return make_response(jsonify({"error": "Si JSON"}), 400)
-    obj = storage.get(State, state_id)
-    if obj is None:
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Update an existing State"""
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated']:
-            setattr(obj, key, value)
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    ignored_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignored_keys:
+            setattr(state, key, value)
     storage.save()
-    return jsonify(obj.to_dict())
+    return jsonify(state.to_dict()), 200
